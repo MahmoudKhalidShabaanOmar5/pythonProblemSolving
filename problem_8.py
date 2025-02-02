6eafaf9ff3ef64980097f8ff655d6ec6b8603894
Mahmoud Khalid Shabaan Omar <m.khalids100200@gmail.com>
2025-02-02 16:26:56 +0300

    Add files via upload

---
+++ problem_8.py
@@ -0,0 +1,6 @@
+# write a python program to get address , and total marks from the user and display in this format : your address is : "address" , and your total marks is = "total marks"=> 
+user_address = input("please enter the address of the user is : ")
+user_total_marks = float(input("please enter the total marks of the user is = "))
+print("the address of the user is : \""+user_address+"\"")
+print("the total marks of the user is = \""+str(user_total_marks)+" marks\"")
+print("your address is : \""+user_address+"\" , and your total marks is = \""+str(user_total_marks)+" marks\"")
