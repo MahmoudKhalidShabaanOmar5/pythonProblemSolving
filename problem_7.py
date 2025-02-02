eaf0ddab592cc4d9dba19869b8fa05c11bbd8a19
Mahmoud Khalid Shabaan Omar <m.khalids100200@gmail.com>
2025-02-02 15:44:08 +0300

    Add files via upload

---
+++ problem_7.py
@@ -0,0 +1,6 @@
+# write a python program to get name , and age from the user , and then display in this format HI "user name" , and your age is "21years"=> 
+user_name = input("please enter the name of the user is : ")
+user_age = float(input("please enter the name of the age is = "))
+print("the name of the user is : \""+user_name+"\"")
+print("the age of the user is = \""+str(user_age)+" years\"")
+print("HI \""+user_name+"\" , and your age is = \""+str(user_age)+" years\"")
